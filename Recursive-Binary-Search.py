52be673985773adb53cb5691772ca8c5c6b25f0b
NishaGoyal-1 <64729885+NishaGoyal-1@users.noreply.github.com>
2020-05-10 20:55:31 +0530

    Add files via upload

--- Recursive-Binary-Search.py
+++ Recursive-Binary-Search.py
@@ -1,19 +1,36 @@
 #program for recursive binary search.return index of x in arr if present,else-1
 def binary_search(arr,left,right,key):
+    #check base case
     mid = left + (right - left)//2
     if right >= left:
+        #if element is present at the middle itself
         if key == arr[mid]:
             return mid
-        elif key > arr[mid]:
-            return binary_search(arr,mid+1,right,key)
-        else:
+        #if element is smaller than mid,then it can only be present in left subarray
+        elif key < arr[mid]:
             return binary_search(arr,left,mid-1,key)
+        #else the element can only be present in right subarray
+        else:
+            return binary_search(arr,mid+1,right,key)
+    else:
+        #element is not present in the array
         return -1
+
+# #driver code
+# arr =[2,3,4,10,40]
+# x =11
+#
+# #function call
+# result = binary_search(arr,0,len(arr)-1,x)
+# if result !=-1:
+#     print("element is present at index %d" % result)
+# else:
+#     print("element is not present in array")
 
 arr = [10,14,19,27,33,35,42,44]
 key =10
-left =0
-right =len(arr)-1
+# left =0
+# right =len(arr)-1
 
 result = binary_search(arr,0,len(arr)-1,33)
 print(result)
