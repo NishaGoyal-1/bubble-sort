9f4f77c3db4e83be4d58df607725991b2677d558
NishaGoyal-1 <64729885+NishaGoyal-1@users.noreply.github.com>
2020-05-12 10:10:32 +0530

    Add files via upload

--- palindrome-number.py
+++ palindrome-number.py
@@ -1,15 +1,14 @@
 # initializing number
 num = 12321
-rev =0
+reverse =0
 x = num
 while num >0:
-    rev = (rev*10) + num%10
+    reverse = (reverse*10) + num%10
     num =num//10
 
 #for checking number is palindrome or not
-if x==rev:
+if x==reverse:
     print("number is palindrome")
 else:
     print("number is not palindrome")
-
 
