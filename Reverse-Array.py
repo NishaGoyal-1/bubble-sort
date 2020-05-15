79b99fc36d6afce6bece224ac4dabf88d6d8eaa8
NishaGoyal-1 <64729885+NishaGoyal-1@users.noreply.github.com>
2020-05-15 07:56:08 +0530

    Add files via upload

--- Reverse-Array.py
+++ Reverse-Array.py
@@ -1,9 +1,9 @@
-# mylist = [14,51,11,12]
-# mylist2 = []
-# for i in (3,2,1,0):
-#     mylist2.append(mylist[i])
-# mylist = mylist2
-# print(mylist)
+mylist = [14,51,11,12]
+mylist2 = []
+for i in (3,2,1,0):
+    mylist2.append(mylist[i])
+mylist = mylist2
+print(mylist)
 
 #second trick
 arr =[1,2,3,4,5]
@@ -18,12 +18,12 @@ def reverse_array(arr):
 reverse_array(arr)
 print(arr)
 
-#copy reverse of array in other array
-# def copy_reverse_array(arr):
-#     destination_arr =[]
-#     for i in reversed(range(len(arr))):
-#         print(i)
-#         destination_arr.append(arr[i])
-#     print(destination_arr)
-#     pass
-# copy_reverse_array(arr)
+copy reverse of array in other array
+def copy_reverse_array(arr):
+    destination_arr =[]
+    for i in reversed(range(len(arr))):
+        print(i)
+        destination_arr.append(arr[i])
+    print(destination_arr)
+    pass
+copy_reverse_array(arr)
