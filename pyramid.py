0060a5a8aa616ebe9cfcaf1506743f1670d9bd94
NishaGoyal-1 <64729885+NishaGoyal-1@users.noreply.github.com>
2020-05-20 23:17:55 +0530

    Add files via upload

---
+++ pyramid.py
@@ -0,0 +1,9 @@
+print("\t\t\t\t\t pyramid of numbers")
+for i in range(1,5):
+    for j in range(5 - i):
+        print(" ", end=" ")
+    for j in range(1, i):
+        print(j, end=" ")
+    for i in range(i, 0, -1):
+        print(i, end=" ")
+    print("\n")
